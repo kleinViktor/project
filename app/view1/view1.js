51b393a08b07736569960271e512fe130b07eb89
zhdm <zhdm@ciklum.com>
2017-11-12 18:54:05 +0200

    Add ability to create author

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,27 +1,85 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute', 'ngStorage'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl',
-    controllerAs: 'ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl',
+            controllerAs: 'ctrl'
+        });
+    }])
 
-.controller('View1Ctrl', [function() {
-  var vm = this;
+    .controller('View1Ctrl', function (userService) {
+        var vm = this;
+        var testRE = new RegExp('([a-zA-Z0-9]){4}');
 
-  vm.authorNameSearch = '';
+        activate();
 
-  var testRE = new RegExp('([a-zA-Z0-9]){4}');
-  vm.startSearch = function(event) {
-    if(testRE.test(vm.authorNameSearch)){
-        alert('aaa!');
-    }
-  }
+        vm.authorNameSearch = '';
 
+        vm.startSearch = function (event) {
+            if (testRE.test(vm.authorNameSearch)) {
+                alert('aaa!');
+            }
+        };
+        vm.newName = '';
+        vm.newSname = '';
+        vm.newDate = '';
+        vm.create = function () {
+            userService.createAuthor(vm.newName, vm.newSname, vm.newDate);
+            initCreateForm();
+        };
 
+        vm.openForm = false;
+        vm.openFormTrigger = function () {
+            vm.openForm = !vm.openForm;
+        };
 
-}]);
+        function activate() {
+            userService.initStorage();
+            initCreateForm();
+        }
+
+        function initCreateForm() {
+            vm.newName = '';
+            vm.newSname = '';
+            vm.newDate = '';
+        }
+
+    })
+
+    .factory('userService', function ($localStorage) {
+        var service = {
+            initStorage: initStorage,
+            createAuthor: createAuthor
+        };
+
+        return service;
+
+        function initStorage() {
+            $localStorage.authors = $localStorage.authors || [];
+            $localStorage.books = $localStorage.books || [];
+            $localStorage.authorId = $localStorage.authorId || $localStorage.authors.length;
+            $localStorage.bookId = $localStorage.bookId || $localStorage.books.length;
+        }
+
+        function createAuthor(name, sname, date, books) {
+            var author = {};
+
+            author.name = name;
+            author.sname = sname;
+            author.date = date;
+            author.books = books || [];
+            author.id = createId();
+
+            function createId() {
+                var id = $localStorage.authorId;
+                $localStorage.authorId = $localStorage.authorId + 1;
+                return id;
+            }
+
+            $localStorage.authors.push(author);
+
+        }
+    });
